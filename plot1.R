a0ea09adc80cdddf7262729d0b1f8a5df4c0e552
nestorov83 <peter.nestorov@yahoo.com>
2015-02-08 13:40:37 +0100

    full script

--- plot1.R
+++ plot1.R
@@ -1,1 +1,13 @@
-sss
+library(sqldf)
+
+
+
+download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="download.zip", method="curl")
+unzip("download.zip")
+data.sub <- read.csv.sql("household_power_consumption.txt", header=TRUE, sep=";", sql="Select * from file where Date = '1/2/2007' OR Date = '2/2/2007'")
+
+data.sub[,3] <- as.numeric(data.sub[,3])
+
+png("plot1.png")
+hist(data.sub[,3], col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
